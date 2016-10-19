6c8d5dd28af0902432ef97c8657c2797a1a515f5
evtuhovaleksander <evtuhov.aleksander@gmail.com>
2016-10-19 17:08:17 +0300

    com3
    com3

---
+++ DocumentAssistant/DocumentAssistant/EditForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DocumentAssistant
+{
+    public partial class EditForm : Form
+    {
+        public EditForm(string Name, string RusName, string Table)
+        {
+            InitializeComponent();
+            dgv.Columns.Clear();
+            dgv.Columns.Add("ID", "ID");
+            dgv.Columns.Add(Name, RusName);
+        }
+
+        private void EditForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
--- DocumentAssistant/DocumentAssistant/Program.cs
+++ DocumentAssistant/DocumentAssistant/Program.cs
@@ -16,7 +16,9 @@ namespace DocumentAssistant
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new EditForm());
+            EditForm frm = new EditForm();
+            frm.ShowDialog();
         }
     }
 }
